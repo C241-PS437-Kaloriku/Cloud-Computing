19064621d4dc7acea94e1e07bb3b264aae0932f5
raianwr <152848027+raianwr@users.noreply.github.com>
2024-06-18 00:27:56 +0700

    Menghapus beberapa code di usersRoutes.js
    Ada beberapa fitur yang dihapus untuk kelancaran pengerjaan

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -9,9 +9,6 @@ const { upload } = require('../users/uploadPPicture');
 // Register
 router.post('/register', usersController.register);
 
-// Request token untuk register
-router.post('/token-register', usersController.tokenRegister);
-
 // Login
 router.post('/login', usersController.login);
 
@@ -30,16 +27,11 @@ router.get('/bmical', checkSession, service.calculatorbmi);
 // Logout akun
 router.delete('/logout', usersController.logout);
 
-// Request token untuk reset password
-router.post('/reset-password-request', usersController.resetPasswordRequest);
-
-// Reset password
-router.post('/reset-password', usersController.resetPassword);
-
 // Save Recommended Foods
 router.post('/save-recommended-foods', checkSession, recommendedFoodsController.saveRecommendedFoods);
 
 // Get Recommended Foods
 router.get('/get-recommended-foods', checkSession, recommendedFoodsController.getRecommendedFoods);
+
 
 module.exports = router;
